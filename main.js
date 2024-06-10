73ec26d92ac06f19ffcbf8c8fd713dab7f3238ab
Said1224nr <162983493+Said1224nr@users.noreply.github.com>
2024-06-10 16:45:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+let input = document.querySelector('input')
+let btn = document.querySelector('button')
+let wrapper = document.querySelector('.wrapper')
+
+let users = [
+    {
+        userName: input.value,
+    }
+]
+
+const ReadFunction = () => {
+    wrapper.innerHTML = ''
+    users.forEach((v) => {
+        let div = document.createElement('div')
+        div.innerHTML = `<div class="rounded-[50%] "></div> <p>${v.userName}</p>`
+        wrapper.appendChild(div)
+    })
+}
+
+
+ReadFunction()
+
+const AddUserFunction = () => {
+    let obj = {
+        id: users.length + 1,
+        userName: input.value
+    }
+
+    users.push(obj)
+    localStorage.setItem('users', JSON.stringify(users))
+    ReadFunction()
+}
+
+btn.addEventListener('click', AddUserFunction)
